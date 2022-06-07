fee8c3209672cbb9175210643a9ad159761f10fa
Mystiick <Mystiick@outlook.com>
2022-06-06 20:39:33 -0500

    Save image data to MySql database

--- Services/DataClient.cs
+++ Services/DataClient.cs
@@ -1,6 +1,8 @@
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
+using MySql.Data.MySqlClient;
+
 using ImageProcessor.Models;
 
 namespace ImageProcessor.Services;
@@ -10,6 +12,7 @@ public class DataClient
     private readonly ILogger<DataClient> _logger;
     private readonly ProcessorConfig _config;
 
+    /// <summary>DI Constructor</summary>
     public DataClient(ILogger<DataClient> logger, IOptions<ProcessorConfig> config)
     {
         _logger = logger;
@@ -18,7 +21,38 @@ public class DataClient
 
     public async Task SaveImageData(List<ImageData> images)
     {
-        _logger.LogInformation("DataClient called, but nothing happened");
-        await Task.CompletedTask;
+        _logger.LogInformation($"Beginning database processing");
+        using var connection = new MySqlConnection(_config.DatabaseConnectionString);
+        await connection.OpenAsync();
+
+        // TODO: Batch insert these instead of one at a time
+        foreach (ImageData img in images)
+        {
+            var command = new MySqlCommand("insert into Image (GUID, ImagePath, ThumbnailPath, Category, SubCategory) values (@GUID, @Image, @Thumbnail, @Category, @SubCategory);", connection);
+
+            command.Parameters.AddWithValue("@GUID", Guid.NewGuid().ToString());
+            command.Parameters.AddWithValue("@Image", img.Image);
+            command.Parameters.AddWithValue("@Thumbnail", img.Thumbnail);
+            command.Parameters.AddWithValue("@Category", img.MetaData.Category);
+            command.Parameters.AddWithValue("@Subcategory", img.MetaData.SubCategory);
+
+            await command.PrepareAsync();
+            await command.ExecuteNonQueryAsync();
+
+            int newID = int.Parse((await new MySqlCommand("select LAST_INSERT_ID() as 'LastID';", connection).ExecuteScalarAsync())?.ToString() ?? "");
+
+            // TODO: Batch insert these instead of one at a time
+            foreach (string tag in img.MetaData.Tags)
+            {
+                var tagCommand = new MySqlCommand("insert into ImageTag (ImageID, TagName) values (@ImageID, @TagName);", connection);
+                tagCommand.Parameters.AddWithValue("@ImageID", newID);
+                tagCommand.Parameters.AddWithValue("@TagName", tag);
+
+                await tagCommand.PrepareAsync();
+                await tagCommand.ExecuteNonQueryAsync();
+            }
+        }
+
+        _logger.LogInformation($"Done with database processing");
     }
 }
